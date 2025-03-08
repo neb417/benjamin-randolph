66653b20c28b86273f516006091c40f55dc5fa8d
Benjamin Randolph <neb417@gmail.com>
2025-03-08 09:06:38 -0700

    Remove flash.

---
+++ app/javascript/controllers/delay_remove_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { amount: { type: Number, default: 2000 } }
+
+  connect() {
+    this.element.classList.add('show-flash')
+    this.timeout = setTimeout(this.#remove, this.amountValue)
+  }
+
+  disconnect() {
+    clearTimeout(this.timeout)
+
+    this.#remove()
+  }
+
+  // private
+
+  #remove = () => {
+    this.element.classList.remove('show-flash')
+    setTimeout(() => {
+      this.element.remove()
+    }, 1000)
+  }
+}
