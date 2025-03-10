b7e7ef41755e3965698d3cd63cd1ea76c24ced45
Benjamin Randolph <neb417@gmail.com>
2025-03-10 16:39:33 -0600

    Remove console logs.

--- app/javascript/controllers/update_skill_icons_controller.js
+++ app/javascript/controllers/update_skill_icons_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log('Controller connected')
     window.addEventListener('resize', this.resize.bind(this))
     this.resize()
   }
@@ -14,10 +13,8 @@ export default class extends Controller {
   resize() {
     const width = window.innerWidth
     const img = document.getElementById('skill-icons')
-    console.log(`width: ${width}`)
 
     if (width < 1168) {
-      console.log(`img src: ${img}`)
       img.src = "https://go-skill-icons.vercel.app/api/icons?i=ruby,rails,postgres,redis,git,heroku,github,githubactions,api,rubymine,vscode,postman,apple,sass,tailwind,bootstrap&perline=16"
     } else {
       img.src = "https://go-skill-icons.vercel.app/api/icons?i=ruby,rails,postgres,redis,git,heroku,github,githubactions,api,rubymine,vscode,postman,apple,sass,tailwind,bootstrap&perline=4"
